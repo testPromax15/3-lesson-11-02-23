a1a1be7e4dbf212e74c5570bc3c1eb712b0ad63a
dati22 <125134310+dati22@users.noreply.github.com>
2023-02-11 17:19:17 +0300

    Add files via upload

---
+++ dati.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=['duck','wave',"horror"]
+b=('duck,wave,horror')
+print(a[0])
+
+
+# In[4]:
+
+
+a=['duck','wave',"horror"]
+print(a[0])
+print(a[1])
+print(a[2])
+
+
+# In[8]:
+
+
+a={'russia':'moscow','usa':'london'}
+print(a['russia'])
+
+
+# In[ ]:
+
+
+
+
