8828f6df72d3576ae8ffe3820b9bf9ad80e7a844
karas228593 <125134445+karas228593@users.noreply.github.com>
2023-02-25 17:45:55 +0300

    Add files via upload

---
+++ Untitled10.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#задание 1
+a=int(input())
+b=int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+
+
+# In[ ]:
+
+
+#задание 2
+a=int(input())
+b=int(input())
+c=int(input())
+
+
+result=((a+2)/(b+5))**4-0.001*c
+
+
+print(result)
+
+
+# In[ ]:
+
+
+#задание 3
+a=input()
+print('='*40)
+print('/t    Сообщение: ' + a)
+print('='*40)
+
+
+# In[ ]:
+
+
+a=("лимоны,апельсины,мандарины")
+b=["лимоны","апельсины","мандарины"]
+print(a[2])
+print(b[2])
+
+
+# In[ ]:
+
+
+#задание 5
+a=[15,78,96,13,25,43]
+del(a[13])
+print(a)
+a.append(4)
+print(a)
+
+
+# In[ ]:
+
+
+#задание 6
+a={"Китай":"Пекин","овощь":"марковь"}
+del(a["овощь"])
+print(a)
+
+
+# In[ ]:
+
+
+a=57
+b=47
+if (a > b):
+    print("a больше чем b")
+
+
+# In[ ]:
+
+
+a=int(input())
+if (a==1)
+    print('')
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+a=int(input())
+if (a==1)
+    print("понедельнтк")
+elif(a==2)
+     print("вторник")
+elif(a==3)
+     print("среда")
+elif(a==4)
+     print("четверг")
+elif(a==5)
+     print("пятница")
+elif(a==6)
+     print("субота")
+elif(a==7)
+     print("воскресение")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+a=int(input())
+print(str(a) + " километра это " + str(a*1000) + "метров")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+a=input()
+print(a.apper())
+
+
+# In[ ]:
+
+
+
+
