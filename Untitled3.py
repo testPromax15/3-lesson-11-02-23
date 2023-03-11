b7ac36848ddd7f3aabad49873846f1952cc4be6d
samjan0709 <125133387+samjan0709@users.noreply.github.com>
2023-03-11 17:23:15 +0300

    Add files via upload

--- Untitled3.py
+++ Untitled3.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[6]:
 
 
-a=['шар','мяч','конфеты']
-b=('шар','мяч')
-print(a[1])
-print(b[1])
-
-
-# In[ ]:
-
-
-a=['шар','мяч','конфеты']
-print(a[1])
-print(b[1])
-
-
-# In[ ]:
-
-
-a={'Россия':'Москва','США','Вашингтон'}
-print(a['Россия'])
-
-
-# In[2]:
-
-
-a={'Россия':'Москва','США':'Вашингтон'}
-print(a['Россия'])
+import matplotlib.pyplot as plt
+x=["Apple","Samsung","HTC","Honor","Xiomi"]
+y=[1095,9966,1095,2300,10000]
+plt.bar(x,y)
+plt.plot(x,y,color="red",label="Линия продаж")
+plt.xlabel("Наименовании компании")
+plt.ylabel("Количество продаж")
+plt.title("Объем продаж")
+plt.grid ()
+plt.text(0,30,"Тренд")
+plt.show
 
 
 # In[ ]:
 
 
+import matplotlib.pyplot as plt
+x=["Apple","Samsung","HTC","Honor","Xiomi"]
+y=[1095,9966,1095,2300,10000]
 
+plt.show
 
